b464d8e3bf41f9ebec6b23093b3221f171626db0
Samuel <samuelrivba@gmail.com>
2024-01-23 14:21:19 -0500

    Add background

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -3,7 +3,9 @@ import Login from "../Components/Login";
 
 const LoginPage = () => {
   return (
-    <div className="w-[100vw] h-[100vh] bg-blue-100 flex items-center justify-center p-10">
+    <div className="w-[100vw] h-[100vh] flex items-center justify-center p-10">
+      <div className="h-full w-full bg-gradient-to-r from-myBlue to-myPink absolute top-0 -z-10 opacity-70" />
+      <div className="h-full w-full absolute bg-pattern -z-20 top-0" />
       <Login />
     </div>
   );
