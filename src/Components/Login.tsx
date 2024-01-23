976b8338f8c631b5aae8f9259a5833d112c8c131
Samuel <samuelrivba@gmail.com>
2024-01-23 14:39:50 -0500

    Add Register form

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,18 +1,42 @@
+import { useState } from "react";
 import Button from "./Button";
 import Input from "./Input";
 
 const Login = () => {
+  const [login, setLogin] = useState(true);
+
   return (
     <div className="w-full md:w-[450px]">
       <h1 className="text-white text-center font-bold text-4xl md:text-6xl mb-10">
-        Login
+        {login ? "Login" : "Register"}
       </h1>
       <div className="bg-white p-6 min-h-[150px] flex flex-col gap-3 w-full rounded-xl drop-shadow-xl">
         <Input name="email" type="email" />
         <Input name="password" type="password" />
-        <Input name="password again" type="password" />
-        <Button text="Login" loading />
-        <Button text="Register" secondary />
+        {!login && <Input name="password again" type="password" />}
+        {login ? (
+          <>
+            <Button text="Login" />
+            <Button
+              text="Register"
+              secondary
+              onClick={() => {
+                setLogin(!login);
+              }}
+            />
+          </>
+        ) : (
+          <>
+            <Button text="Register" />
+            <Button
+              text="Login"
+              secondary
+              onClick={() => {
+                setLogin(!login);
+              }}
+            />
+          </>
+        )}
       </div>
     </div>
   );
