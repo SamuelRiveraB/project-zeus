73dd54679a858e394bdfb10aa35978f7e0b312d6
Samuel <samuelrivba@gmail.com>
2024-01-25 22:29:43 -0500

    Add Sidebar active tab

--- src/Components/SideBarLeft.tsx
+++ src/Components/SideBarLeft.tsx
@@ -1,20 +1,45 @@
 import React from "react";
 import Sidebar from "./Sidebar";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../Redux/store";
+import { setIsChatsTab } from "../Redux/chatsSlice";
+import Chats from "./Chats";
+import Users from "./Users";
 
 type Props = {};
 
 function SideBarLeft({}: Props) {
+  const isChatsTab = useSelector((state: RootState) => state.chat.isChatsTab);
+  const dispatch = useDispatch<AppDispatch>();
   return (
-    <Sidebar className={`flex-[0.8] w-[80%] h-[80%] md:h-full md:w-full`}>
+    <Sidebar className={`flex-[0.4] w-[80%] h-[80%] md:h-full md:w-full`}>
       <div className="flex flex-col">
         <div className="flex sticky top-0 z-10">
-          <p className={`p-5 flex-1 text-center font-bold cursor-pointer`}>
+          <p
+            onClick={() => dispatch(setIsChatsTab(true))}
+            className={`p-5 flex-1 text-center font-bold cursor-pointer ${
+              isChatsTab
+                ? "bg-gradient-to-r from-myBlue to-myPink text-white"
+                : "bg-gray-200 text-gray-900"
+            }`}
+          >
             Chats
           </p>
+
+          <p
+            onClick={() => dispatch(setIsChatsTab(false))}
+            className={`p-5 flex-1 text-center font-bold cursor-pointer ${
+              !isChatsTab
+                ? "bg-gradient-to-r from-myBlue to-myPink text-white"
+                : "bg-gray-200 text-gray-900"
+            }`}
+          >
+            Users
+          </p>
         </div>
-        <p className={`p-5 flex-1 text-center font-bold cursor-pointer`}>
-          Users
-        </p>
+        <div className="flex-1 flex flex-col py-2 max-h-ful overflow-scroll">
+          {isChatsTab ? <Chats /> : <Users />}
+        </div>
       </div>
     </Sidebar>
   );
--- src/Redux/chatsSlice.ts
+++ src/Redux/chatsSlice.ts
@@ -1,11 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = { isChatsTab: false, chats: [] };
 
 const chatsSlice = createSlice({
   name: "chat",
   initialState,
-  reducers: {},
+  reducers: {
+    setIsChatsTab: (state, action) => {
+      state.isChatsTab = action.payload;
+    },
+  },
 });
 
+export const { setIsChatsTab } = chatsSlice.actions;
 export default chatsSlice.reducer;
