c6ba46be06ee3e8508a77af7d43589b68c456ba1
Samuel <samuelrivba@gmail.com>
2024-01-25 19:44:02 -0500

    Generate Random Avatar

--- src/Pages/ProfilePage.tsx
+++ src/Pages/ProfilePage.tsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Input from "../Components/Input";
 import Button from "../Components/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../Redux/store";
+import avatarGenerator from "../utils/avatarGenerator";
 
 function ProfilePage() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPW, setConfirmPW] = useState("");
+  const [avatar, setAvatar] = useState("");
+
+  const currentUser = useSelector((state: RootState) => state.user.currentUser);
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    setEmail(currentUser.email);
+    setUsername(currentUser.username);
+  }, [currentUser]);
+
+  const handleAvatarGenerate = () => {
+    setAvatar(avatarGenerator());
+  };
+
   return (
     <div className="bg-white flex flex-col gap-5 shadow-md max-w-2xl rounded-xl py-5 px-6 md:p-10 md:m-auto m-5 md:mt-10">
-      <div className="relative self-center">
+      <div className="relative self-center" onClick={handleAvatarGenerate}>
         <img
-          src="https://api.multiavatar.com/sam.png"
-          alt=""
+          src={avatar || currentUser.img}
+          alt={currentUser.username}
           className="w-32 h-32 md:w-48 md:h-48 rounded-full p-[2px] ring-2 ring-gray-300 cursor-pointer hover:shadow-lg"
         />
         <span className="absolute top-7 md:top-6 left-28 md:left-40 w-5 h-5 border-2 border-gray-800 rounded-full bg-green-400"></span>
@@ -17,10 +38,26 @@ function ProfilePage() {
         Note: Click on image to temporary change it
       </p>
       <div className="flex flex-col gap-2">
-        <Input name="email" />
-        <Input name="username" />
-        <Input name="password" />
-        <Input name="email" />
+        <Input
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value.trim())}
+        />
+        <Input
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value.trim())}
+        />
+        <Input
+          name="password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value())}
+        />
+        <Input
+          name="confirmPW"
+          type="password"
+          onChange={(e) => setConfirmPW(e.target.value())}
+        />
         <Button text="Update Profile" />
         <Button text="Delete Account" secondary />
       </div>
--- src/utils/avatarGenerator.ts
+++ src/utils/avatarGenerator.ts
@@ -1,3 +1,5 @@
+import { generate } from "random-words";
+
 export default function avatarGenerator(text?: string) {
-  return `https://api.multiavatar.com/${text || "random"}.png`;
+  return `https://api.multiavatar.com/${text || generate()}}.png`;
 }
